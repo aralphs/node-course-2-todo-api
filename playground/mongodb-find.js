2b1f1b802794c50d9c0ca87bd018aa14b8dd46ab
Anthony Ralphs <aralphs@hotmail.com>
2017-05-13 15:57:18 +0100

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,41 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+
+// console.log(ob);
+
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to database');
+  }
+  console.log('Connected to the database');
+
+  db.collection('Users').find({
+    name: 'Anthony'
+  }).toArray().then((docs) => {
+    console.log('Users');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch Users', err);
+  });
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID ('591718454461bd27bc7c9693')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.close();
+});
